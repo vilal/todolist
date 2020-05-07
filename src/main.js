907d7e65afea0f4a0239d46d2cb64cf34c449605
vilal <lalandev@free.fr>
2020-05-07 16:44:08 +0200

    ADD [vue font awesome] useful icons

--- src/main.js
+++ src/main.js
@@ -1,5 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faList,
+  faCalendar,
+  faStar,
+  faSearch,
+  faSlidersH,
+  faRedo,
+  faThumbtack,
+  faExclamation
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
+library.add(
+  faList,
+  faCalendar,
+  faStar,
+  faSearch,
+  faSlidersH,
+  faRedo,
+  faThumbtack,
+  faExclamation
+)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
