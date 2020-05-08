d013da1792e9bf8a1851524ecbfaae2f8f2acf7e
vilal <lalandev@free.fr>
2020-05-08 19:22:53 +0200

    SETUP [App] add task functioning

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,24 @@
 
       div
         font-awesome-icon(icon="plus")
-        span Ajouter une tâche
+        span Add a task
+
+      form(@submit.prevent="addTodo")
+        label Titre
+        input(type="text" v-model="title")
+        label Description
+        input(type="text" v-model="description")
+        label Durée
+        input(type="text" v-model="duration")
+        input(type="checkbox" v-model="recurrent")
+        label Récurrent
+        input(type="checkbox" v-model="marked")
+        label Epinglé
+        input(type="checkbox" v-model="priority")
+        label Prioritaire
+        input(type="checkbox" v-model="team")
+        label En équipe
+        input(type="submit" value="Envoyer")
 
 </template>
 
@@ -18,14 +35,44 @@ import TodoList from "./components/todo/TodoList.vue"
 import datas from './datas/todos.json'
 
 export default {
-  name: 'App',
   components: {
     MainHeader,
     TodoList,
   },
   data() {
     return {
-      todos: datas
+      todos: datas,
+      title: '',
+      description: '',
+      duration: '',
+      recurrent: false,
+      marked: false,
+      priority: false,
+      team: false,
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        title: this.title,
+        description: this.description,
+        duration: this.duration,
+        recurrent: this.recurrent,
+        marked: this.marked,
+        priority: this.priority,
+        team: this.team
+      }).then(
+        this.initForm()
+      )
+    },
+    initForm() {
+      this.title = ''
+      this.description = ''
+      this.duration = ''
+      this.recurrent = false
+      this.marked = false
+      this.priority = false
+      this.team = false
     }
   }
 }
